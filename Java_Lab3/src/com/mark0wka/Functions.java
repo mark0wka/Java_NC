8dc73de47106c8b818900344daef92db1c79d03c
Владимир Чистов <wowulina@yandex.ru>
2021-11-26 04:32:47 +0300

    newcommit

---
+++ Java_Lab3/src/com/mark0wka/Functions.java
@@ -0,0 +1,126 @@
+package com.mark0wka;
+
+import com.mark0wka.collections.MyLinkedList;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+public class Functions {
+
+    public static void listTimings(List<Integer> list, int numbers) {
+        System.out.println("****List testing****");
+        long startTime = System.nanoTime();
+        for (int i = 0; i < numbers; i++) {
+            list.add(numbers - i);
+        }
+        long stopTime = System.nanoTime();
+        double result = stopTime - startTime;
+        System.out.println("Filling time in milliseconds: " + result / 1000000);
+
+        startTime = System.nanoTime();
+        list.add(numbers/12, numbers * 2);
+        stopTime = System.nanoTime();
+        result = stopTime - startTime;
+        System.out.println("Adding time in milliseconds: " + result / 1000000);
+
+        startTime = System.nanoTime();
+        int get = list.get(numbers/6);
+        stopTime = System.nanoTime();
+        result = stopTime - startTime;
+        System.out.println("Getting time in milliseconds: " + result / 1000000);
+
+        startTime = System.nanoTime();
+        list.remove(numbers/7);
+        stopTime = System.nanoTime();
+        result = stopTime - startTime;
+        System.out.println("Removing time in milliseconds: " + result / 1000000);
+    }
+
+    public static void setTimings(Set<Integer> set, int numbers) {
+        System.out.println("****Set testing****");
+        long startTime = System.nanoTime();
+        for (int i = 0; i < numbers; i++) {
+            set.add(numbers - i);
+        }
+        long stopTime = System.nanoTime();
+        double result = stopTime - startTime;
+        System.out.println("Filling time in milliseconds: " + result / 1000000);
+
+        startTime = System.nanoTime();
+        set.add(numbers * 2);
+        stopTime = System.nanoTime();
+        result = stopTime - startTime;
+        System.out.println("Adding time in milliseconds: " + result / 1000000);
+
+        startTime = System.nanoTime();
+        set.contains(numbers/4);
+        stopTime = System.nanoTime();
+        result = stopTime - startTime;
+        System.out.println("Contains time in milliseconds: " + result / 1000000);
+
+        startTime = System.nanoTime();
+        set.remove(numbers/7);
+        stopTime = System.nanoTime();
+        result = stopTime - startTime;
+        System.out.println("Removing time in milliseconds: " + result / 1000000);
+    }
+
+    public static void mapTimings(Map<Integer, Integer> map, int numbers) {
+        System.out.println("****Map testing****");
+        long startTime = System.nanoTime();
+        for (int i = 0; i < numbers; i++) {
+            map.put(i, numbers - i);
+        }
+        long stopTime = System.nanoTime();
+        double result = stopTime - startTime;
+        System.out.println("Filling time in milliseconds: " + result / 1000000);
+
+        startTime = System.nanoTime();
+        map.get(numbers / 10);
+        stopTime = System.nanoTime();
+        result = stopTime - startTime;
+        System.out.println("Getting time in milliseconds: " + result / 1000000);
+
+        startTime = System.nanoTime();
+        map.putIfAbsent(numbers + 2, 1234);
+        stopTime = System.nanoTime();
+        result = stopTime - startTime;
+        System.out.println("putIfAbsent time in milliseconds: " + result / 1000000);
+
+        startTime = System.nanoTime();
+        map.remove(numbers/12);
+        stopTime = System.nanoTime();
+        result = stopTime - startTime;
+        System.out.println("Removing time in milliseconds: " + result / 1000000);
+    }
+
+    public static void myListTimings(MyLinkedList<Integer> list, int numbers) {
+        System.out.println("****List testing****");
+        long startTime = System.nanoTime();
+        for (int i = 0; i < numbers; i++) {
+            list.add(numbers - i);
+        }
+        long stopTime = System.nanoTime();
+        double result = stopTime - startTime;
+        System.out.println("Filling time in milliseconds: " + result / 1000000);
+
+        startTime = System.nanoTime();
+        list.add(numbers/12, numbers * 2);
+        stopTime = System.nanoTime();
+        result = stopTime - startTime;
+        System.out.println("Adding time in milliseconds: " + result / 1000000);
+
+        startTime = System.nanoTime();
+        int get = list.get(numbers/6);
+        stopTime = System.nanoTime();
+        result = stopTime - startTime;
+        System.out.println("Getting time in milliseconds: " + result / 1000000);
+
+        startTime = System.nanoTime();
+        list.remove(numbers/7);
+        stopTime = System.nanoTime();
+        result = stopTime - startTime;
+        System.out.println("Removing time in milliseconds: " + result / 1000000);
+    }
+}
---
+++ Java_Lab3/src/com/mark0wka/Main.java
@@ -0,0 +1,80 @@
+package com.mark0wka;
+
+import com.mark0wka.collections.MyLinkedList;
+
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        MyLinkedList<Integer> myLinkedList = new MyLinkedList<Integer>();
+        List<Integer> arrayList = new ArrayList<>();
+        List<Integer> linkerList = new LinkedList<>();
+        Set<Integer> hashSet = new HashSet<>();
+        Set<Integer> linkedHashSet = new LinkedHashSet<>();
+        Set<Integer> treeSet = new TreeSet<>();
+        Map<Integer, Integer> hashMap = new HashMap<>();
+        Map<Integer, Integer> linkedHashMap = new LinkedHashMap<>();
+        Map<Integer, Integer> treeMap = new TreeMap<>();
+
+        for (int i = 0; i < 15; i++) {
+            myLinkedList.add(i);
+        }
+
+        myLinkedList.add(15, 15);
+        System.out.println(myLinkedList.size());
+        System.out.println(myLinkedList.get(2));
+        System.out.println(myLinkedList.toString());
+        System.out.println(myLinkedList.indexOf(15));
+        System.out.println(myLinkedList.indexOf(0));
+        System.out.println(myLinkedList.indexOf(6));
+        System.out.println(myLinkedList.remove(7));
+        System.out.println(myLinkedList.size());
+        myLinkedList.add(7, 654);
+        System.out.println(myLinkedList.set(7, 7));
+        System.out.println(myLinkedList.toString());
+        Integer[] array = new Integer[16];
+        array = myLinkedList.toArray(new Integer[0]);
+        for (int i = 0; i < array.length; i++) {
+            System.out.println(array[i]);
+        }
+        Integer[] array1 = new Integer[16];
+        myLinkedList.toArray(array1);
+        for (int i = 0; i < array1.length; i++) {
+            System.out.println(array1[i]);
+        }
+        myLinkedList.clear();
+
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        int numbers = 10000;
+
+        MyLinkedList<Integer> myLinkedList1 = new MyLinkedList<>();
+
+        for (int i = 0; i < 4; i++) {
+            numbers = 10000 * (int)Math.pow(10, i);
+            System.out.println("****testing MyLinkedList and LinkedList with " + numbers + " elements");
+            Functions.myListTimings(myLinkedList1, numbers);
+            Functions.listTimings(linkerList, numbers);
+            System.out.println();
+        }
+
+        for (int i = 0; i < 3; i++) {
+            numbers = 10000 * (int)Math.pow(10, i);
+            System.out.println("****testing with " + numbers + " elements");
+            Functions.listTimings(arrayList, numbers);
+            Functions.listTimings(linkerList, numbers);
+            System.out.println();
+            Functions.setTimings(hashSet, numbers);
+            Functions.setTimings(linkedHashSet, numbers);
+            Functions.setTimings(treeSet, numbers);
+            System.out.println();
+            Functions.mapTimings(hashMap, numbers);
+            Functions.mapTimings(linkedHashMap, numbers);
+            Functions.mapTimings(treeMap, numbers);
+            System.out.println();
+        }
+    }
+}
